bc941f02fa3c54d901be207695c773230ec83228
Andrey Nekrasov <bonerdelli@gmail.com>
2024-09-10 18:19:17 +0200

    Remove unused module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  providers: [HttpClient],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'stock-screener';
+  title = 'Stock Screener Application';
 }
