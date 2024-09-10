c6fdae7b6fb9a0613c75df6443df1e4b93337ae6
Andrey Nekrasov <bonerdelli@gmail.com>
2024-09-10 20:27:16 +0200

    Update table layout, add more columns

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule
+  ],
+  providers: [],
+})
+export class AppModule { }
